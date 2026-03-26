8199f05b98bb25ad1e411b15bb2c58f1649cb512
Douglas Sampaio <douglassampaio23@gmail.com>
2026-03-26 14:48:30 -0300

    added selectors list

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,15 +1,24 @@
 describe('.orangehrm tests', () => {
-  it.skip('Login - Success', () => {
+
+  const selectorsList = {
+    usernameField: "[name ='username']",
+    passwordField: '[name="password"]',
+    loginButton: '.oxd-button',
+    sectionTitleTopBar: '.oxd-topbar-header-title',
+    wrongCredentialAlert: "[role='alert']"
+  } 
+
+  it('Login - Success', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
-    cy.get('[name="username"]').type('Admin')
-    cy.get('[name="password"]').type('admin123')
-    cy.get('.oxd-button').click()
-    cy.get('.oxd-topbar-header-title').contains('Dashboard')
+    cy.get(selectorsList.usernameField).type('Admin')
+    cy.get(selectorsList.passwordField).type('admin123')
+    cy.get(selectorsList.loginButton).click()
+    cy.get(selectorsList.sectionTitleTopBar).contains('Dashboard')
     
   })
 
   
-  it('Login - Fail', () => {
+  it.skip('Login - Fail', () => {
     cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
     cy.get('[name="username"]').type('Adminn')
     cy.get('[name="password"]').type('admin123')
